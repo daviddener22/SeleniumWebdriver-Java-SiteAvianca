1d634a5aa6c87af70682ab79458b105574cad768
cgr <cgr@CGR004.intranet.leste>
2018-08-06 07:44:45 -0300

    Teste automatizado com SeleniumWebdriver em Java

---
+++ src/test/java/tests/InformacoesAviancaEn.java
@@ -0,0 +1,61 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class InformacoesAviancaEn {
+
+    private WebDriver navegador;
+
+    @Before
+    public void setUP(){
+        //Abrindo o navegador
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\daviddener_silva\\drivers\\chromedriver.exe");
+        navegador = new ChromeDriver();
+        navegador.manage().window().maximize();//Abrir o navegador maximizado
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);//Aguardar 5 seg. para aplicação carregar os elementos para interagirmos com eles
+
+        //Navegando para a página principal da Avianca para selecionar o idioma;
+        navegador.get("https://www.avianca.com.br/");
+    }
+
+    @Test
+    public void testVerificarIdiomaEn() {
+
+       //Seleciona o idioma no RadioButton
+        navegador.findElement(By.xpath("//label[@for='en_US']")).click();
+
+       //Clica no botão Enviar para abrir a página com o idioma selecionado
+        navegador.findElement(By.xpath("//*[text()='Enviar']")).click();
+
+       //Identifica o texto no menu superior no idioma selecionado
+        WebElement TextoMenuSuperior = navegador.findElement(By.linkText("Your Trip"));
+
+       //Verifica se o linktext "Your Trip" está disponível, caso ainda não esteja, aguarda 10 seg. até ficar disponível para o click
+        WebDriverWait aguardar = new WebDriverWait(navegador, 10);
+        aguardar.until(ExpectedConditions.elementToBeClickable(TextoMenuSuperior));
+
+       //Atribuindo o valor de TextoMenuSuperior para textoMenu para ser validado
+        String textoMenu = TextoMenuSuperior.getText();
+
+        //Valida se o texto do textlink do menu superior é "Your Trip"
+         assertEquals("Your Trip", textoMenu);
+    }
+
+    @After
+    public void tearDown(){
+        //Fechar o navegador
+        navegador.quit();
+    }
+}
---
+++ src/test/java/tests/InformacoesAviancaEs.java
@@ -0,0 +1,61 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class InformacoesAviancaEs {
+
+    private WebDriver navegador;
+
+    @Before
+    public void setUP(){
+        //Abrindo o navegador
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\daviddener_silva\\drivers\\chromedriver.exe");
+        navegador = new ChromeDriver();
+        navegador.manage().window().maximize();//Abrir o navegador maximizado
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);//Aguardar 5 seg. para aplicação carregar os elementos para interagirmos com eles
+
+        //Navegando para a página principal da Avianca para selecionar o idioma;
+        navegador.get("https://www.avianca.com.br/");
+    }
+
+    @Test
+    public void testVerificarIdiomaEs() {
+
+        //Seleciona o idioma no RadioButton
+        navegador.findElement(By.xpath("//label[@for='es_ES']")).click();
+
+        //Clica no botão Enviar para abrir a página com o idioma selecionado
+        navegador.findElement(By.xpath("//*[text()='Enviar']")).click();
+
+        //Identifica o texto no menu superior no idioma selecionado
+        WebElement TextoMenuSuperior = navegador.findElement(By.linkText("Su Viaje"));
+
+        //Verifica se o linktext "Su Viaje" está disponível, caso ainda não esteja, aguarda 10 seg. até ficar disponível para o click
+        WebDriverWait aguardar = new WebDriverWait(navegador, 10);
+        aguardar.until(ExpectedConditions.elementToBeClickable(TextoMenuSuperior));
+
+        //Atribuindo o valor de TextoMenuSuperior para textoMenu para ser validado
+        String textoMenu = TextoMenuSuperior.getText();
+
+        //Valida se o texto do textlink do menu superior é "Su Viaje"
+        assertEquals("Su Viaje", textoMenu);
+    }
+
+    @After
+    public void tearDown(){
+        //Fechar o navegador
+        navegador.quit();
+    }
+}
